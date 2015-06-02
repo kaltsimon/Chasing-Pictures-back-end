83bd50e6086b84b6a18f4da06c9369208db64df3
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-06-02 15:55:11 +0200

    Allow the name parameter to be submitted at sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: { registrations: 'auth/registrations' }
 end
