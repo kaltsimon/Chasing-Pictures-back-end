679b7184e7a63b48957f58bbe27b656de1894659
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-06-08 23:24:34 +0200

    Get geo data from YAML file.

--- generate_data.rb
+++ generate_data.rb
@@ -3,25 +3,29 @@ require 'open-uri'
 require 'digest/md5'
 require 'yaml'
 
-# seems to work http://stackoverflow.com/questions/1026337/how-do-i-get-link-to-an-image-on-wikipedia-from-the-infobox
-def commons_page_to_image_url(file_url)
-  file_name = file_url[40..-1]
-  digest = Digest::MD5.hexdigest(URI.unescape(file_name))
+GEO_DATA = YAML::load_file (File.dirname(__FILE__) + "/db/geo_data.yml")
 
-  "http://upload.wikimedia.org/wikipedia/commons/#{digest[0]}/#{digest[0..1]}/#{file_name}"
+# seems to work http://stackoverflow.com/questions/1026337/how-do-i-get-link-to-an-image-on-wikipedia-from-the-infobox
+def commons_data(file_url)
+  file_name = URI.unescape(file_url.split(/File\:|\//).last)
+  digest = Digest::MD5.hexdigest(file_name)
+
+  [file_name, "http://upload.wikimedia.org/wikipedia/commons/#{digest[0]}/#{digest[0..1]}/#{file_name}"]
 end
 
 doc = Nokogiri::XML(open('https://offenedaten.de/storage/f/2014-04-06T10%3A06%3A29.535Z/stadtmuseumberlin-stadtansichten.xml')).remove_namespaces!
 places, pictures = [], []
 
 doc.css('museumdat').each do |item|
+  file_name, image_url = commons_data(item.xpath('.//resourceID').text)
+
   places << {
     name: item.xpath('.//title[@pref="preferred"]').text,
     description: item.xpath('.//title[@pref="alternate"]').text,
-    longitude: 0.0,
-    latitude: 0.0,
+    longitude: GEO_DATA[file_name]['lon'],
+    latitude: GEO_DATA[file_name]['lat'],
     picture: {
-      url: commons_page_to_image_url(item.xpath('.//resourceID').text),
+      url: image_url,
       time: Date.new((item.xpath('.//earliestDate').empty? ? item.xpath('.//displayCreationDate').text : item.xpath('.//earliestDate').text).to_i),
     }
   }
