27e2453b8f903bffb64ae87a31e3ac9659b7af40
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-06-17 11:12:23 +0200

    Add paperclip.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,7 @@
 class Picture < ActiveRecord::Base
   belongs_to :place
+
+  has_attached_file :file, styles: { medium: '800x800>', thumb: '100x100>' }
+  validates_attachment_presence :file
+  validates_attachment_content_type :file, content_type: /\Aimage\/.*\Z/
 end
