256bebb8b7a1a7128eedc3eff8186dd3d7b2899d
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-06-26 20:10:03 +0200

    Download original images for seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
+require 'open-uri'
+
 YAML::load_file(File.dirname(__FILE__) + '/seed_data.yml').each do |item|
   place = Place::where(item.except(:picture)).first_or_create
-  Picture::where(item[:picture].merge(place: place)).first_or_create
+  Picture::where(item[:picture].merge(place: place)).first_or_create do |picture|
+    url = item[:picture][:url]
+    puts "Downloading #{url}..."
+
+    picture.url = nil
+    picture.file = open(URI.encode(url))
+  end
 end
